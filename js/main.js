1ead1c1a1311673b5b1b3d4ea98867ab938c3563
iqbalriiaz <iqbalriiaz@gmail.com>
2023-03-31 19:01:40 +0600

    launching new site

---
+++ js/main.js
@@ -0,0 +1,48 @@
+var menu = document.getElementById('menu');
+var nav = document.getElementById('nav');
+var exit = document.getElementById('exit');
+
+// menu.addEventListener('click', function (e) {
+//     nav.classList.toggle('hide-mobile');
+//     e.preventDefault();
+// });
+
+// exit.addEventListener('click', function (e) {
+//     nav.classList.add('hide-mobile');
+//     e.preventDefault();
+// });
+
+// scroll gifs starts ----------------------------
+// var gifScroll = document.querySelector('.gif-scroll');
+// var scrollStartX;
+// var scrollStartY;
+// var scrollX;
+// var scrollY;
+// var isDragging = false;
+
+// gifScroll.addEventListener('mousedown', function (e) {
+//     scrollStartX = e.pageX - gifScroll.offsetLeft;
+//     scrollStartY = e.pageY - gifScroll.offsetTop;
+//     isDragging = true;
+//     gifScroll.classList.add('dragging');
+// });
+
+// gifScroll.addEventListener('mousemove', function (e) {
+//     if (!isDragging) return;
+//     e.preventDefault();
+//     scrollX = e.pageX - gifScroll.offsetLeft;
+//     scrollY = e.pageY - gifScroll.offsetTop;
+//     var scrollDist = scrollX - scrollStartX;
+//     gifScroll.scrollLeft -= scrollDist;
+//     scrollStartX = scrollX;
+// });
+
+// gifScroll.addEventListener('mouseup', function (e) {
+//     isDragging = false;
+//     gifScroll.classList.remove('dragging');
+// });
+
+
+// scroll gifs ends ----------------------------
+
+// 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
