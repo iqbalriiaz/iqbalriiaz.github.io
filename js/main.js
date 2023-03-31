e6816a793a1ddb121f8c41b891c5c9f062dba300
iqbalriiaz <iqbalriiaz@gmail.com>
2023-04-01 00:37:15 +0600

    refactored files

--- js/main.js
+++ js/main.js
@@ -2,62 +2,12 @@ var menu = document.getElementById('menu');
 var nav = document.getElementById('nav');
 var exit = document.getElementById('exit');
 
-// if (jQuery(".Light").length > 0) {
-//     jQuery(".Light").lightGallery({
-//         selector: "a",
-//     });
-// }
+menu.addEventListener('click', function (e) {
+    nav.classList.toggle('hide-mobile');
+    e.preventDefault();
+});
 
-// //------------ Light gallery with thumbnail
-// if (jQuery(".LightThumb").length > 0) {
-//     jQuery(".LightThumb").lightGallery({
-//         selector: "a",
-//         exThumbImage: "data-exthumbimage",
-//     });
-// }
-
-
-// menu.addEventListener('click', function (e) {
-//     nav.classList.toggle('hide-mobile');
-//     e.preventDefault();
-// });
-
-// exit.addEventListener('click', function (e) {
-//     nav.classList.add('hide-mobile');
-//     e.preventDefault();
-// });
-
-// scroll gifs starts ----------------------------
-// var gifScroll = document.querySelector('.gif-scroll');
-// var scrollStartX;
-// var scrollStartY;
-// var scrollX;
-// var scrollY;
-// var isDragging = false;
-
-// gifScroll.addEventListener('mousedown', function (e) {
-//     scrollStartX = e.pageX - gifScroll.offsetLeft;
-//     scrollStartY = e.pageY - gifScroll.offsetTop;
-//     isDragging = true;
-//     gifScroll.classList.add('dragging');
-// });
-
-// gifScroll.addEventListener('mousemove', function (e) {
-//     if (!isDragging) return;
-//     e.preventDefault();
-//     scrollX = e.pageX - gifScroll.offsetLeft;
-//     scrollY = e.pageY - gifScroll.offsetTop;
-//     var scrollDist = scrollX - scrollStartX;
-//     gifScroll.scrollLeft -= scrollDist;
-//     scrollStartX = scrollX;
-// });
-
-// gifScroll.addEventListener('mouseup', function (e) {
-//     isDragging = false;
-//     gifScroll.classList.remove('dragging');
-// });
-
-
-// scroll gifs ends ----------------------------
-
-// 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
+exit.addEventListener('click', function (e) {
+    nav.classList.add('hide-mobile');
+    e.preventDefault();
+});
